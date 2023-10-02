6b3803fe7b86d5a5b088346810afab624825b910
Judith Leonardo <90572650+judith91@users.noreply.github.com>
2023-10-02 12:17:37 -0300

    Primer commit

---
+++ functions.js
@@ -0,0 +1,5 @@
+document.getElementById("botonDesplegable").addEventListener("click", function() {
+    var lista = document.getElementById("listaEnlaces");
+    lista.classList.toggle("active");
+  });
+  
